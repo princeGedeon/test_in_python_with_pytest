bb756fd6bbb9b31cf0e917644dc9aaf6b2c17f8d
princeGedeon <guedjegedeon03@gmail.com>
2022-10-04 10:21:52 +0100

    coverage init

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #Test additioonner deux nombres
 
 #Test additionner chaines de caracteres
-from main import add
+from main import add, divide
 
 from unittest import TestCase
 
@@ -18,5 +18,16 @@ class TestCalculatrice(TestCase):
         self.assertEqual(add(False,False),0)
         self.assertEqual(add(True,False),1)
         self.assertEqual(add(False,True),1)
+
+    def test_with_none(self):
+        with self.assertRaises(TypeError):
+            add(None,None)
+
+    def test_devide_obtain_virgule(self):
+        self.assertEqual(divide(5,2),2.5)
+
+    def test_cas_critic(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(5,0)
 
 
