80e54025f7e9e2fd6f612a4513feaf32b003886e
princeGedeon <guedjegedeon03@gmail.com>
2022-10-04 10:34:42 +0100

    pytest

--- test.py
+++ test.py
@@ -4,30 +4,32 @@
 from main import add, divide
 
 from unittest import TestCase
+import pytest
 
 
-class TestCalculatrice(TestCase):
-    def test_add_two_numbers(self):
-        self.assertEqual(add(5,10),15)
 
-    def test_add_two_string(self):
-        self.assertEqual(add('a','b'),'ab')
+def test_add_two_numbers():
+    assert add(5,10)==15
 
-    def test_add_boolean(self):
-        self.assertEqual(add(True,True),2)
-        self.assertEqual(add(False,False),0)
-        self.assertEqual(add(True,False),1)
-        self.assertEqual(add(False,True),1)
+def test_add_two_string():
+    assert add('a','b')=='ab'
 
-    def test_with_none(self):
-        with self.assertRaises(TypeError):
-            add(None,None)
+def test_add_boolean():
+    assert add(True,True)==2
+    assert add(False,False)==0
+    assert add(True,False)==1
+    assert add(False,True)==1
 
-    def test_devide_obtain_virgule(self):
-        self.assertEqual(divide(5,2),2.5)
+def test_with_none():
+    with pytest.raises(TypeError):
+        add(None, None)
 
-    def test_cas_critic(self):
-        with self.assertRaises(ZeroDivisionError):
-            divide(5,0)
+
+def test_devide_obtain_virgule():
+    assert divide(5,2)==2.5
+
+def test_cas_critic():
+    with pytest.raises(ZeroDivisionError):
+        divide(5,0)
 
 
